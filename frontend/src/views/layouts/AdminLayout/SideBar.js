5bc52397152e52e6828a3fbd7eaafc85c2840bb2
StreamDotMy <streamdotmysolutions@gmail.com>
2024-10-24 21:52:38 +0800

    [AdminLayout] update SideBar

--- frontend/src/views/layouts/AdminLayout/SideBar.js
+++ frontend/src/views/layouts/AdminLayout/SideBar.js
@@ -20,49 +20,6 @@ const SideBar = () => {
                     <Link to="/admin/users" className="nav-link text-dark-custom px-sm-0 px-2">
                         <i className="fs-5 bi-people"></i><span className="ms-1 d-none d-sm-inline">User</span>
                     </Link>
-{/* 
-                    <Link to="/admin/livestream" className="nav-link text-dark-custom px-sm-0 px-2">
-                        <i className="fs-5 bi-camera"></i><span className="ms-1 d-none d-sm-inline text-muted">Livestream</span>
-                    </Link>
-
-                    <Link to="/admin/recording" className="nav-link text-dark-custom px-sm-0 px-2">
-                        <i className="fs-5 bi-database"></i><span className="ms-1 d-none d-sm-inline">Recording</span>
-                    </Link> */}
-
-                    <Link to="/admin/topics" className="nav-link text-dark-custom px-sm-0 px-2">
-                        <i className="fs-5 bi-p-circle"></i><span className="ms-1 d-none d-sm-inline">Carta Undi</span>
-                    </Link>
-
-                    {/* <Link to="/admin/chatroom" className="nav-link text-dark-custom px-sm-0 px-2">
-                        <i className="fs-5 bi-chat"></i><span className="ms-1 d-none d-sm-inline">Chatroom</span>
-                    </Link> */}
-
-                
-
-                    <Link to="/admin/banners" className="nav-link text-dark-custom px-sm-0 px-2">
-                        <i className="fs-5 bi-image"></i><span className="ms-1 d-none d-sm-inline">Banner</span>
-                    </Link>
-
-                    <Link to="/admin/deejays" className="nav-link text-dark-custom px-sm-0 px-2">
-                        <i className="fs-5 bi-person"></i><span className="ms-1 d-none d-sm-inline">Deejay</span>
-                    </Link>
-
-                    {/* <Link to="/admin/banners" className="nav-link text-dark-custom px-sm-0 px-2">
-                        <i className="fs-5 bi-clipboard"></i><span className="ms-1 d-none d-sm-inline">News</span>
-                    </Link> */}
-
-                    {/* <Link to="/admin/banners" className="nav-link text-dark-custom px-sm-0 px-2">
-                        <i className="fs-5 bi-image"></i><span className="ms-1 d-none d-sm-inline">Gallery</span>
-                    </Link> */}
-
-                    {/* <Link to="/admin/banners" className="nav-link text-dark-custom px-sm-0 px-2">
-                        <i className="fs-5 bi-code"></i><span className="ms-1 d-none d-sm-inline">Header</span>
-                    </Link> */}
-
-                    <Link to="/admin/footer" className="nav-link text-dark-custom px-sm-0 px-2">
-                        <i className="fs-5 bi-code"></i><span className="ms-1 d-none d-sm-inline">Footer</span>
-                    </Link> 
-
                     
                 </li>
             </ul>
